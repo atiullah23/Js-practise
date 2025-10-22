f38440f97640266608d886b819fb6b88d8b24192
atiullah23 <ashmirvlog786@gmail.com>
2025-10-22 08:58:35 +0545

    Add some code

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+// if else statement
+// exmaple1
 let age=17;
 if(age>=18){
     console.log("you can vote");
 }
 if (age<18){
     console.log("you cannot vote");
+}
+// exmaple2
+let mode="dark";
+if(mode=dark){
+    console.log("The Background color is in Dark Mode");
+} else{
+    console.log("The Background color still in previous mode");
+}
+// exmaple3
+let switch1="on";
+if(switch1=on){
+    console.log("Tubelight is in on mode");
+}else{
+    console.log("Tublight still in off mode");
+}
+// exmaple4
+let Fan="GreenButton";
+if(Fan=GreenButton){
+    console.log("Fan is moving in on mode");
+}else{
+    console.log("Fan is not moves in off mode");
+}
+// example5
+let Bike="Key";
+if(Bike=Key){
+    console.log("Bike is in Start Position");
+}else{
+    console.log("Bike is in stop position");
 }
