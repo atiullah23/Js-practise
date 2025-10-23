13bd92255ad3ece06a2be9c1e975adcba2d13a94
atiullah23 <ashmirvlog786@gmail.com>
2025-10-23 08:25:26 +0545

    Add loop file

---
+++ loop.js
@@ -0,0 +1,17 @@
+// for loop
+// exmaple1
+for(let i=1;1<=5;i++){
+    console.log("Er Ashmir Khan");
+}
+// exmaple2
+let Name;
+let num;
+for(let num=1;num<=6;num=num+1){
+Name="Er Ashmir Khan";
+}
+console.log(Name);
+
+// exmaple3
+for(let j=1;j<=5;j+1){
+    console.log("J value are:",j);
+}
